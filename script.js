f535b3129415012f9668220baca78c6f14be7d5e
Akram Ansari <119692799+akram222001@users.noreply.github.com>
2024-03-29 00:38:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
-}
+function indexOfIgnoreCase(str, subStr) {
+  if (str === null || subStr === null || str.length === 0 || subStr.length === 0) {
+    return -1; // Handle edge cases like empty strings
+  }
 
-// Please do not change the code below
-const s1 = prompt("Enter s1:");
-const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+  str = str.toLowerCase();
+  subStr = subStr.toLowerCase();
+
+  return str.indexOf(subStr);
+}
