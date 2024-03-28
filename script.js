b780d4fac1f6f7e13888d725be4a18b51619fb5c
Akram Ansari <119692799+akram222001@users.noreply.github.com>
2024-03-29 00:39:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ function indexOfIgnoreCase(str, subStr) {
 
   return str.indexOf(subStr);
 }
+
+// Example usage:
+// console.log(indexOfIgnoreCase("Hello World", "world")); // Output: 6
+// console.log(indexOfIgnoreCase("apple", "Ple")); // Output: 2
+// console.log(indexOfIgnoreCase("test", "aaa")); // Output: -1
+
+// Please do not change the code below
+const s1 = prompt("Enter s1:");
+const s2 = prompt("Enter s2:");
+alert(indexOfIgnoreCase(s1, s2));
